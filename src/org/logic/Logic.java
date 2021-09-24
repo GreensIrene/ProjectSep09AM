f2fc2f2a333afc6f482b03d43ccb0515e84e0d3a
GreensIrene <albeenairene28@gmail.com>
2021-09-24 12:13:45 +0530

    Username Functionality

--- src/org/logic/Logic.java
+++ src/org/logic/Logic.java
@@ -2,4 +2,11 @@ package org.logic;
 
 public class Logic {
 
+	public static void userName() {
+		System.out.println("Username");
+	}
+	
+	public static void main(String[] args) {
+		userName();
+	}
 }
