42fbddf38af453bf429feb933c001c7703221c03
GreensIrene <albeenairene28@gmail.com>
2021-09-24 12:49:49 +0530

    Password FUnctionalty

--- src/org/logic/Logic.java
+++ src/org/logic/Logic.java
@@ -2,4 +2,11 @@ package org.logic;
 
 public class Logic {
 
+	public static void passWord() {
+		System.out.println("password functionality");
+}
+	
+	public static void main(String[] args) {
+		passWord();
+	}
 }
